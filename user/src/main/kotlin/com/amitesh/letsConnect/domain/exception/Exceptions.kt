f8512738b62e9cc32bcfe563517985885b5f6bf6
amiteshsinghkundan <amiteshsinghk@gmail.com>
2025-10-06 01:35:00 +0530

    1. Register user.

--- user/src/main/kotlin/com/amitesh/letsConnect/domain/exception/Exceptions.kt
+++ user/src/main/kotlin/com/amitesh/letsConnect/domain/exception/Exceptions.kt
@@ -2,18 +2,6 @@ package com.amitesh.letsConnect.domain.exception
 
 import java.lang.RuntimeException
 
-class UserAlreadyExistException: RuntimeException(
-    "A user with this email or username is already exists."
-)
-
-class UsernameEmptyException: RuntimeException(
-    "Username shouldn't be empty."
-)
-
-class EmailEmptyException: RuntimeException(
-    "Email shouldn't be empty."
-)
-
-class PasswordEmptyException: RuntimeException(
-    "Password shouldn't be empty."
+class UserAlreadyExistException(user: String?): RuntimeException(
+    "A user with this $user is already exists."
 )
--- user/src/main/kotlin/com/amitesh/letsConnect/service/auth/AuthService.kt
+++ user/src/main/kotlin/com/amitesh/letsConnect/service/auth/AuthService.kt
@@ -1,29 +1,29 @@
-package com.amitesh.letsConnect.infra.security
+package com.amitesh.letsConnect.service.auth
 
-import com.amitesh.letsConnect.domain.exception.EmailEmptyException
-import com.amitesh.letsConnect.domain.exception.PasswordEmptyException
 import com.amitesh.letsConnect.domain.exception.UserAlreadyExistException
-import com.amitesh.letsConnect.domain.exception.UsernameEmptyException
 import com.amitesh.letsConnect.domain.model.User
 import com.amitesh.letsConnect.infra.database.entities.UserEntity
 import com.amitesh.letsConnect.infra.database.mappers.toUser
 import com.amitesh.letsConnect.infra.database.repositories.UserRepository
+import com.amitesh.letsConnect.infra.security.PasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service
 class AuthService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder
 ) {
-    fun register(email: String, username: String, password: String): User{
-        require(email.isNotEmpty()) { throw EmailEmptyException() }
-        require(username.isNotEmpty()) { throw UsernameEmptyException() }
-        require(password.isNotEmpty()) { throw PasswordEmptyException() }
-
+    fun register(email: String, username: String, password: String): User {
         val userExist = userRepository.findByEmailOrUsername(
             email = email.trim(),
             username = username.trim()
         )
         if (userExist != null){
-            throw UserAlreadyExistException()
+            when{
+                userExist.email.equals(email, ignoreCase = true) -> throw UserAlreadyExistException("email '${userExist.email}'" )
+                userExist.username.equals(username, ignoreCase = true) -> throw UserAlreadyExistException("username '${userExist.username}'")
+            }
+
         }
         val saveduser = userRepository.save(
             UserEntity(
