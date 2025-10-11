dd4de43891dd3b3e09baa29077c0c3fb02162f12
amiteshsinghkundan <amiteshsinghk@gmail.com>
2025-10-11 13:14:27 +0530

    1.User Login Service

--- user/src/main/kotlin/com/amitesh/letsConnect/service/auth/AuthService.kt
+++ user/src/main/kotlin/com/amitesh/letsConnect/service/auth/AuthService.kt
@@ -1,17 +1,28 @@
 package com.amitesh.letsConnect.service.auth
 
+import com.amitesh.letsConnect.domain.exception.InvalidCredentialException
 import com.amitesh.letsConnect.domain.exception.UserAlreadyExistException
+import com.amitesh.letsConnect.domain.exception.UserNotFoundException
+import com.amitesh.letsConnect.domain.model.AuthenticatedUser
 import com.amitesh.letsConnect.domain.model.User
+import com.amitesh.letsConnect.domain.model.UserId
+import com.amitesh.letsConnect.infra.database.entities.RefreshTokenEntity
 import com.amitesh.letsConnect.infra.database.entities.UserEntity
 import com.amitesh.letsConnect.infra.database.mappers.toUser
+import com.amitesh.letsConnect.infra.database.repositories.RefreshTokenRepository
 import com.amitesh.letsConnect.infra.database.repositories.UserRepository
 import com.amitesh.letsConnect.infra.security.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.Base64
 
 @Service
 class AuthService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
     fun register(email: String, username: String, password: String): User {
         val userExist = userRepository.findByEmailOrUsername(
@@ -33,5 +44,49 @@ class AuthService(
             )
         ).toUser()
         return saveduser
+    }
+
+    fun login(
+        email: String,
+        password: String
+    ): AuthenticatedUser {
+        val user = userRepository.findByEmail(email = email.trim())
+            ?: throw InvalidCredentialException()
+
+        if(!passwordEncoder.matches(password, user.hashedPassword)) {
+            throw InvalidCredentialException()
+        }
+
+        //TODO: Check for verified email
+
+        return  user.id?.let { userId ->
+            val accessToken = jwtService.generateAccessToken(userId = userId)
+            val refreshToken = jwtService.generateRefreshToken(userId = userId)
+            storeRefreshToken(userId, refreshToken)
+            AuthenticatedUser(
+                user = user.toUser(),
+                accessToken = accessToken,
+                refreshToken = refreshToken
+            )
+        } ?: throw UserNotFoundException()
+    }
+
+    private fun storeRefreshToken(userId: UserId, token: String) {
+        val hashed = hashedToken(token)
+        val expiryMs = jwtService.refreshTokenValidityMs
+        val expiresAt = Instant.now().plusMillis(expiryMs)
+        refreshTokenRepository.save(
+            RefreshTokenEntity(
+                userId = userId,
+                expiresAt = expiresAt,
+                hashedToken = hashed
+            )
+        )
+    }
+
+    private fun hashedToken(token: String): String{
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashedBytes = digest.digest(token.encodeToByteArray())
+        return Base64.getEncoder().encodeToString(hashedBytes)
     }
 }
